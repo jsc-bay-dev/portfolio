67932c46167447ccca4b36b7011cdbda5455817d
Justin Choi <j@Justins-MBP.lan>
2025-07-30 07:50:54 -0700

    vibes

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,31 @@ import About from './components/About'
 import Blog from './components/Blog'
 import Projects from './components/Projects'
 import Credentials from './components/Credentials'
+import Vibes from './components/Vibes'
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Header />
-      <main className="space-y-16">
-        <section id="about" className="bg-white">
-          <About />
-        </section>
-        <section id="credentials" className="bg-gray-50 py-16">
-          <Credentials />
-        </section>
-        <section id="projects" className="bg-white">
-          <Projects />
-        </section>
-        <section id="blog" className="bg-gray-50">
-          <Blog />
-        </section>
-        <section id="contact" className="bg-white">
-          <Contact />
-        </section>
-      </main>
-    </div>
+    <Vibes/>
+    // <div className="min-h-screen bg-gray-50">
+    //   <Header />
+    //   <main className="space-y-16">
+    //     <section id="about" className="bg-white">
+    //       <About />
+    //     </section>
+    //     <section id="credentials" className="bg-gray-50 py-16">
+    //       <Credentials />
+    //     </section>
+    //     <section id="projects" className="bg-white">
+    //       <Projects />
+    //     </section>
+    //     <section id="blog" className="bg-gray-50">
+    //       <Blog />
+    //     </section>
+    //     <section id="contact" className="bg-white">
+    //       <Contact />
+    //     </section>
+    //   </main>
+    // </div>
   )
 }
 
